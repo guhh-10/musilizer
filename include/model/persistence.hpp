c187e5f23f98e1a9cf117735cfc1f3499ff57518
guhh-10 <naufal.ikhwansyah10@gmail.com>
2026-05-31 21:27:27 +0700

    refactor(persistence): move API to namespace

--- include/model/persistence.hpp
+++ include/model/persistence.hpp
@@ -4,13 +4,12 @@
 #include "model/playlist.hpp"
 #include "model/play_history.hpp"
 
-class Persistence{
-    public:
-        static void init();
-        static void saveSettings(float volume, bool shuffle, bool repeat);
-        static void loadSettings(float& volume, bool& shuffle, bool& repeat);
-        static void savePlaylists(const std::vector<Playlist>& playlists);
-        static std::vector<Playlist> loadPlaylists(const Library& lib);
-        static void saveHistory(const PlayHistory& history);
-        static void loadHistory(PlayHistory& history, const Library& lib);
-};
+namespace Persistence {
+    void init();
+    void saveSettings(float volume, bool shuffle, bool repeat);
+    void loadSettings(float& volume, bool& shuffle, bool& repeat);
+    void savePlaylists(const std::vector<Playlist>& playlists);
+    std::vector<Playlist> loadPlaylists(const Library& lib);
+    void saveHistory(const PlayHistory& history);
+    void loadHistory(PlayHistory& history, const Library& lib);
+}
